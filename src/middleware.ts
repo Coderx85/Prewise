2a11127e2c7e3419bc51637a10373cdedb61f408
Coderx85 <work.priyanshu085@gmail.com>
2025-04-11 11:29:44 +0530

    refactor: remove arcjet integration and related rate limiting logic from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import arcjet, { shield } from '@arcjet/next';
 import { NextResponse } from 'next/server';
 
 const protectedRoute = createRouteMatcher(['/interview/:path*']);
 
-const API_KEY: string = process.env.NEXT_PUBLIC_ARCJET_KEY || '';
-
-const aj = arcjet({
-  key: API_KEY,
-  rules: [
-    shield({
-      mode: 'LIVE',
-    }),
-  ],
-});
-
 export default clerkMiddleware(async (auth, req) => {
-  const check = await aj.protect(req);
-
-  if (check.isDenied()) {
-    return NextResponse.json('Rate limit exceeded', { status: 403 });
-  }
-
   if (protectedRoute(req)) await auth.protect();
 
   return NextResponse.next();
